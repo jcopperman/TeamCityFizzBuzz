792f2c4b8421a127636299283f6f4e7bf33e1678
jcopperman <opperman.jonathan@gmail.com>
2016-02-29 12:03:32 +0200

    initial

---
+++ TeamCityFizzBuzz/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TeamCityFizzBuzz.Startup))]
+namespace TeamCityFizzBuzz
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
